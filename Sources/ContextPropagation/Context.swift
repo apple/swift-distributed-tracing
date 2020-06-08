b5236270089de6768e38f0a49b8b09d44906a3ed
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2020-06-08 17:56:59 +0900

    +sanity #17 add sanity scripts (formatting, license)

--- Sources/ContextPropagation/Context.swift
+++ Sources/ContextPropagation/Context.swift
@@ -8,22 +8,22 @@ public struct Context {
     public init() {}
 
     public mutating func inject<Key: ContextKey>(_ key: Key.Type, value: Key.Value) {
-        dict[ObjectIdentifier(key)] = ValueContainer(value: value)
+        self.dict[ObjectIdentifier(key)] = ValueContainer(value: value)
     }
 
     public func extract<Key: ContextKey>(_ key: Key.Type) -> Key.Value? {
-        dict[ObjectIdentifier(key)]?.forceUnwrap(key)
+        self.dict[ObjectIdentifier(key)]?.forceUnwrap(key)
     }
 
     public mutating func remove<Key: ContextKey>(_ key: Key.Type) {
-        dict[ObjectIdentifier(key)] = nil
+        self.dict[ObjectIdentifier(key)] = nil
     }
 
     private struct ValueContainer {
         let value: Any
 
         func forceUnwrap<Key: ContextKey>(_ key: Key.Type) -> Key.Value {
-            value as! Key.Value
+            self.value as! Key.Value
         }
     }
 }
