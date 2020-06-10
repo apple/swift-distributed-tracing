f4cd38c2a994f01357262ab27b04ccd1ba630fe1
Moritz Lang <hi@slashmo.codes>
2020-06-10 18:56:47 +0200

    Remove shared Context in NIOInstrumentation

--- Sources/NIOInstrumentation/Context+Shared.swift
+++
@@ -1,6 +0,0 @@
-import ContextPropagation
-
-extension Context {
-    /// Singleton `Context` instance to be used in `NIO` until we can pass around `Context` through `NIO`.
-    public static var shared = Context()
-}
