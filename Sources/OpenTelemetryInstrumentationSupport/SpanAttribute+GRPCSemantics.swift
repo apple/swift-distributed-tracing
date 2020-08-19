ab120672829cefdeb3c932ffb41e013da120b896
Moritz Lang <hi@slashmo.codes>
2020-08-19 17:09:18 +0200

    Add gRPC message semantic attributes

---
+++ Sources/OpenTelemetryInstrumentationSupport/SpanAttribute+GRPCSemantics.swift
@@ -0,0 +1,71 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Distributed Tracing open source project
+//
+// Copyright (c) 2020 Moritz Lang and the Swift Tracing project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import TracingInstrumentation
+
+extension SpanAttributeName {
+    /// - See: GRPCAttributes
+    public static let gRPCMessageType = "message.type"
+    /// - See: GRPCAttributes
+    public static let gRPCMessageID = "message.id"
+    /// - See: GRPCAttributes
+    public static let gRPCMessageCompressedSize = "message.compressed_size"
+    /// - See: GRPCAttributes
+    public static let gRPCMessageUncompressedSize = "message.uncompressed_size"
+}
+
+#if swift(>=5.2)
+extension SpanAttributes {
+    /// Semantic conventions for gRPC spans.
+    public var gRPC: GRPCAttributes {
+        get {
+            .init(attributes: self)
+        }
+        set {
+            self = newValue.attributes
+        }
+    }
+}
+
+/// Semantic conventions for gRPC spans as defined in the OpenTelemetry spec.
+///
+/// - SeeAlso: [OpenTelemetry: Semantic conventions for gRPC spans](https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/rpc.md#grpc) (as of August 2020)
+@dynamicMemberLookup
+public struct GRPCAttributes: SpanAttributeNamespace {
+    public var attributes: SpanAttributes
+
+    public init(attributes: SpanAttributes) {
+        self.attributes = attributes
+    }
+
+    public struct NestedAttributes: NestedSpanAttributesProtocol {
+        public init() {}
+
+        /// The type of message, e.g. "SENT" or "RECEIVED".
+        public var messageType: SpanAttributeKey<String> { .init(name: SpanAttributeName.gRPCMessageType) }
+
+        /// The message id calculated as two different counters starting from 1, one for sent messages and one for received messages.
+        public var messageID: SpanAttributeKey<Int> { .init(name: SpanAttributeName.gRPCMessageID) }
+
+        /// The compressed message size in bytes.
+        public var messageCompressedSize: SpanAttributeKey<Int> {
+            .init(name: SpanAttributeName.gRPCMessageCompressedSize)
+        }
+
+        /// The uncompressed message size in bytes.
+        public var messageUncompressedSize: SpanAttributeKey<Int> {
+            .init(name: SpanAttributeName.gRPCMessageUncompressedSize)
+        }
+    }
+}
+#endif
