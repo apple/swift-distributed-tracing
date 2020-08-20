77e77c850120048c011607f4fb4c64b3b06b78be
Moritz Lang <hi@slashmo.codes>
2020-08-20 10:27:38 +0200

    Namespace (g)RPC SpanAttributeNames

--- Sources/OpenTelemetryInstrumentationSupport/SpanAttribute+GRPCSemantics.swift
+++ Sources/OpenTelemetryInstrumentationSupport/SpanAttribute+GRPCSemantics.swift
@@ -15,13 +15,16 @@ import TracingInstrumentation
 
 extension SpanAttributeName {
     /// - See: GRPCAttributes
-    public static let gRPCMessageType = "message.type"
-    /// - See: GRPCAttributes
-    public static let gRPCMessageID = "message.id"
-    /// - See: GRPCAttributes
-    public static let gRPCMessageCompressedSize = "message.compressed_size"
-    /// - See: GRPCAttributes
-    public static let gRPCMessageUncompressedSize = "message.uncompressed_size"
+    public enum GRPC {
+        /// - See: GRPCAttributes
+        public static let messageType = "message.type"
+        /// - See: GRPCAttributes
+        public static let messageID = "message.id"
+        /// - See: GRPCAttributes
+        public static let messageCompressedSize = "message.compressed_size"
+        /// - See: GRPCAttributes
+        public static let messageUncompressedSize = "message.uncompressed_size"
+    }
 }
 
 #if swift(>=5.2)
@@ -52,19 +55,19 @@ public struct GRPCAttributes: SpanAttributeNamespace {
         public init() {}
 
         /// The type of message, e.g. "SENT" or "RECEIVED".
-        public var messageType: SpanAttributeKey<String> { .init(name: SpanAttributeName.gRPCMessageType) }
+        public var messageType: SpanAttributeKey<String> { .init(name: SpanAttributeName.GRPC.messageType) }
 
         /// The message id calculated as two different counters starting from 1, one for sent messages and one for received messages.
-        public var messageID: SpanAttributeKey<Int> { .init(name: SpanAttributeName.gRPCMessageID) }
+        public var messageID: SpanAttributeKey<Int> { .init(name: SpanAttributeName.GRPC.messageID) }
 
         /// The compressed message size in bytes.
         public var messageCompressedSize: SpanAttributeKey<Int> {
-            .init(name: SpanAttributeName.gRPCMessageCompressedSize)
+            .init(name: SpanAttributeName.GRPC.messageCompressedSize)
         }
 
         /// The uncompressed message size in bytes.
         public var messageUncompressedSize: SpanAttributeKey<Int> {
-            .init(name: SpanAttributeName.gRPCMessageUncompressedSize)
+            .init(name: SpanAttributeName.GRPC.messageUncompressedSize)
         }
     }
 }
