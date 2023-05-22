ef5d3dbef566c30c5fa12eadc9601b83b4f1f1b4
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-05-22 21:35:47 +0200

    Adjust licensing headers

--- Samples/Dinner/Sources/Onboarding/Dinner.swift
+++ Samples/Dinner/Sources/Onboarding/Dinner.swift
@@ -1,5 +1,18 @@
 //===----------------------------------------------------------------------===//
 //
+// This source file is part of the Swift Distributed Tracing open source project
+//
+// Copyright (c) 2020-2023 Apple Inc. and the Swift Distributed Tracing project
+// authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//
 // This source file is part of the Swift OpenTelemetry open source project
 //
 // Copyright (c) 2021 Moritz Lang and the Swift OpenTelemetry project authors
@@ -10,6 +23,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
 
 import Tracing
 
