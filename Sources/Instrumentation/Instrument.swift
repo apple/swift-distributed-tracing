582fff5ea77d7e09e81a7cca87f6ae377bc21a6d
Joe Heck <j_heck@apple.com>
2025-09-26 08:42:52 -0700

    cleaned up the sentences around carriers, knowledge of types, and injecting and extracting values

--- Sources/Instrumentation/Instrument.swift
+++ Sources/Instrumentation/Instrument.swift
@@ -19,8 +19,9 @@ import ServiceContextModule
 /// The assocaited type, `Carrier`, is a service request such as an HTTP request,
 /// that has string values that can be extracted to provide information for a tracing span.
 ///
-/// Typically the library adopting instrumentation, would provide an implementation of this type, since it is aware of its carrier type.
-/// Which then can be combined with instrumentation or tracing implementations, which are not aware of the concrete carrier, and only provide an ``Instrument`` (or `Tracer`) which makes use of injector/extractor to operate on carrier values.
+/// Typically the library adopting instrumentation provides an implementation of this type, since it is aware of its carrier type.
+/// That type can be combined with instrumentation or tracing implementations, which are not aware of the concrete carrier,
+/// and only provide an ``Instrument`` (or `Tracer`) which makes use of injector/extractor to operate on carrier values.
 public protocol Extractor: Sendable {
     /// The carrier to extract values from.
     associatedtype Carrier: Sendable
@@ -36,9 +37,10 @@ public protocol Extractor: Sendable {
 /// A type that allows you to inject values to an associated carrier.
 ///
 /// The associated type, `Carrier`, is often a client or outgoing request into which values are inserted for tracing spans.
-
-/// Typically the library adopting instrumentation, would provide an implementation of this type, since it is aware of its carrier type.
-/// Which then can be combined with instrumentation or tracing implementations, which are not aware of the concrete carrier, and only provide an ``Instrument`` (or `Tracer`) which makes use of injector/extractor to operate on carrier values.
+/// 
+/// Typically the library adopting instrumentation provides an implementation of this type, since it is aware of its carrier type.
+/// That type can be combined with instrumentation or tracing implementations, which are not aware of the concrete carrier,
+/// and only provide an ``Instrument`` (or `Tracer`) which makes use of injector/extractor to operate on carrier values.
 public protocol Injector: Sendable {
     /// The carrier to inject values into.
     associatedtype Carrier: Sendable
